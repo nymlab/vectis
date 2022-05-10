c0a28a938f39dd0d0e8714b4779a6bf4530fe4b0
bwty <whalelephant@users.noreply.github.com>
2022-05-10 09:44:58 -0700

    fix: increase jasmine timeout

--- js-app/spec/helpers/helper.js
+++ js-app/spec/helpers/helper.js
@@ -3,5 +3,6 @@
 const TSConsoleReporter = require("jasmine-ts-console-reporter");
 jasmine.getEnv().clearReporters(); //Clear default console reporter
 jasmine.getEnv().addReporter(new TSConsoleReporter());
-jasmine.getEnv().configure({random: false});
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 60*1000;
+// This is high because each suite may take some time and if it is too low
+// the suite will time out and start the next suite without warning.
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 600*1000;
