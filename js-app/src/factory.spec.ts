08b7462ac4a5f3984928cfac7680e6074f03a6cb
bwty <whalelephant@users.noreply.github.com>
2022-05-10 00:23:31 -0700

    fix: uncomment

--- js-app/src/factory.spec.ts
+++ js-app/src/factory.spec.ts
@@ -25,110 +25,110 @@ import { defaultWalletCreationFee } from "./util/fee";
 /**
  * This suite tests Factory contract methods
  */
-// describe("Factory Suite: ", () => {
-//     let adminClient: SigningCosmWasmClient;
-//     let client: CosmWasmClient;
-//     let proxyCodeId: number;
-//     let multisigCodeId: number;
-//
-//     let factoryClient: FactoryClient;
-//     let proxyWalletAddress: Addr;
-//
-//     beforeAll(async () => {
-//         try {
-//             adminClient = await createSigningClient(adminMnemonic!, addrPrefix!);
-//             client = await CosmWasmClient.connect(rpcEndPoint!);
-//             const { factoryRes, proxyRes, multisigRes, govecRes } = await uploadContracts(adminClient);
-//             proxyCodeId = proxyRes.codeId;
-//             multisigCodeId = multisigRes.codeId;
-//
-//             const { factoryAddr } = await instantiateFactoryContract(
-//                 adminClient,
-//                 factoryRes.codeId,
-//                 proxyCodeId,
-//                 multisigCodeId
-//             );
-//             factoryClient = new FactoryClient(adminClient, adminAddr!, factoryAddr);
-//
-//             const { govecAddr } = await instantiateGovec(adminClient, govecRes.codeId, factoryAddr);
-//
-//             await factoryClient.updateGovecAddr({ addr: govecAddr });
-//             let govec = await factoryClient.govecAddr();
-//             expect(govec).toEqual(govecAddr);
-//         } catch (err) {
-//             console.error("Failed to load scenario!", err);
-//         }
-//     });
-//
-//     beforeEach(() => {
-//         assert(adminClient);
-//         assert(client);
-//         assert(factoryClient);
-//     });
-//
-//     it("Should have correct funds in Factory contract", async () => {
-//         const fund = await client.getBalance(factoryClient.contractAddress, coinMinDenom!);
-//         expect(fund).toEqual(FACTORY_INITIAL_FUND);
-//     });
-//
-//     it("Should store Proxy code id in Factory contract", async () => {
-//         const codeId = await client.queryContractSmart(factoryClient.contractAddress, {
-//             code_id: { ty: "Proxy" },
-//         });
-//         expect(codeId).toEqual(proxyCodeId);
-//     });
-//
-//     it("Should create a new proxy wallet with multisig", async () => {
-//         const userKeypair = await mnemonicToKeyPair(userMnemonic!);
-//         const walletCreationFee = await factoryClient.fee();
-//         const totalFee: Number = Number(walletCreationFee.amount) + Number(testWalletInitialFunds.amount);
-//
-//         const newWalletRes = await factoryClient.createWallet(
-//             {
-//                 createWalletMsg: {
-//                     user_pubkey: toBase64(userKeypair.pubkey),
-//                     guardians: {
-//                         addresses: [guardian1Addr!, guardian2Addr!],
-//                         guardians_multisig: {
-//                             threshold_absolute_count: 1,
-//                             multisig_initial_funds: [],
-//                         },
-//                     },
-//                     relayers: [relayer1Addr!, relayer2Addr!],
-//                     proxy_initial_funds: [testWalletInitialFunds as Coin],
-//                 },
-//             },
-//             defaultWalletCreationFee,
-//             undefined,
-//             [coin(totalFee.toString(), coinMinDenom!) as Coin]
-//         );
-//
-//         const wasmEvent = newWalletRes.logs[0].events.length;
-//         const wasmEventAttributes = newWalletRes.logs[0].events[wasmEvent - 1].attributes;
-//         proxyWalletAddress = wasmEventAttributes.find((event) => event.key === "proxy_address")?.value!;
-//
-//         expect(proxyWalletAddress).toBeTruthy();
-//     });
-//
-//     it("Should find created proxy wallet in Factory contract", async () => {
-//         // By calling 'wallets'
-//         const { wallets } = await factoryClient.wallets({});
-//         expect(wallets.length).toEqual(1);
-//         expect(wallets[0]).toEqual(proxyWalletAddress);
-//
-//         // By calling 'wallets_of' with user address
-//         const { wallets: userWallets } = await factoryClient.walletsOf({ user: userAddr! });
-//         expect(userWallets.length).toEqual(1);
-//         expect(userWallets[0]).toEqual(proxyWalletAddress);
-//     });
-//
-//     it("Should get correct balance in proxy wallet", async () => {
-//         const balance = await client.getBalance(proxyWalletAddress, coinMinDenom!);
-//         expect(balance).toEqual(testWalletInitialFunds);
-//     });
-//
-//     afterAll(() => {
-//         adminClient?.disconnect();
-//         client?.disconnect();
-//     });
-// });
+describe("Factory Suite: ", () => {
+    let adminClient: SigningCosmWasmClient;
+    let client: CosmWasmClient;
+    let proxyCodeId: number;
+    let multisigCodeId: number;
+
+    let factoryClient: FactoryClient;
+    let proxyWalletAddress: Addr;
+
+    beforeAll(async () => {
+        try {
+            adminClient = await createSigningClient(adminMnemonic!, addrPrefix!);
+            client = await CosmWasmClient.connect(rpcEndPoint!);
+            const { factoryRes, proxyRes, multisigRes, govecRes } = await uploadContracts(adminClient);
+            proxyCodeId = proxyRes.codeId;
+            multisigCodeId = multisigRes.codeId;
+
+            const { factoryAddr } = await instantiateFactoryContract(
+                adminClient,
+                factoryRes.codeId,
+                proxyCodeId,
+                multisigCodeId
+            );
+            factoryClient = new FactoryClient(adminClient, adminAddr!, factoryAddr);
+
+            const { govecAddr } = await instantiateGovec(adminClient, govecRes.codeId, factoryAddr);
+
+            await factoryClient.updateGovecAddr({ addr: govecAddr });
+            let govec = await factoryClient.govecAddr();
+            expect(govec).toEqual(govecAddr);
+        } catch (err) {
+            console.error("Failed to load scenario!", err);
+        }
+    });
+
+    beforeEach(() => {
+        assert(adminClient);
+        assert(client);
+        assert(factoryClient);
+    });
+
+    it("Should have correct funds in Factory contract", async () => {
+        const fund = await client.getBalance(factoryClient.contractAddress, coinMinDenom!);
+        expect(fund).toEqual(FACTORY_INITIAL_FUND);
+    });
+
+    it("Should store Proxy code id in Factory contract", async () => {
+        const codeId = await client.queryContractSmart(factoryClient.contractAddress, {
+            code_id: { ty: "Proxy" },
+        });
+        expect(codeId).toEqual(proxyCodeId);
+    });
+
+    it("Should create a new proxy wallet with multisig", async () => {
+        const userKeypair = await mnemonicToKeyPair(userMnemonic!);
+        const walletCreationFee = await factoryClient.fee();
+        const totalFee: Number = Number(walletCreationFee.amount) + Number(testWalletInitialFunds.amount);
+
+        const newWalletRes = await factoryClient.createWallet(
+            {
+                createWalletMsg: {
+                    user_pubkey: toBase64(userKeypair.pubkey),
+                    guardians: {
+                        addresses: [guardian1Addr!, guardian2Addr!],
+                        guardians_multisig: {
+                            threshold_absolute_count: 1,
+                            multisig_initial_funds: [],
+                        },
+                    },
+                    relayers: [relayer1Addr!, relayer2Addr!],
+                    proxy_initial_funds: [testWalletInitialFunds as Coin],
+                },
+            },
+            defaultWalletCreationFee,
+            undefined,
+            [coin(totalFee.toString(), coinMinDenom!) as Coin]
+        );
+
+        const wasmEvent = newWalletRes.logs[0].events.length;
+        const wasmEventAttributes = newWalletRes.logs[0].events[wasmEvent - 1].attributes;
+        proxyWalletAddress = wasmEventAttributes.find((event) => event.key === "proxy_address")?.value!;
+
+        expect(proxyWalletAddress).toBeTruthy();
+    });
+
+    it("Should find created proxy wallet in Factory contract", async () => {
+        // By calling 'wallets'
+        const { wallets } = await factoryClient.wallets({});
+        expect(wallets.length).toEqual(1);
+        expect(wallets[0]).toEqual(proxyWalletAddress);
+
+        // By calling 'wallets_of' with user address
+        const { wallets: userWallets } = await factoryClient.walletsOf({ user: userAddr! });
+        expect(userWallets.length).toEqual(1);
+        expect(userWallets[0]).toEqual(proxyWalletAddress);
+    });
+
+    it("Should get correct balance in proxy wallet", async () => {
+        const balance = await client.getBalance(proxyWalletAddress, coinMinDenom!);
+        expect(balance).toEqual(testWalletInitialFunds);
+    });
+
+    afterAll(() => {
+        adminClient?.disconnect();
+        client?.disconnect();
+    });
+});
--- js-app/src/proxy.spec.ts
+++ js-app/src/proxy.spec.ts
@@ -35,229 +35,229 @@ import {
 /**
  * This suite tests Proxy contract methods
  */
-// describe("Proxy Suite: ", () => {
-//     let userClient: SigningCosmWasmClient;
-//     let adminClient: SigningCosmWasmClient;
-//     let client: CosmWasmClient;
-//     let proxyWalletAddress: Addr;
-//
-//     let factoryClient: FactoryClient;
-//     let proxyClient: ProxyClient;
-//
-//     async function createTestProxyWallet(): Promise<Addr> {
-//         assert(factoryClient);
-//         const userKeypair = await mnemonicToKeyPair(userMnemonic!);
-//         const walletCreationFee = await factoryClient.fee();
-//         const totalFee: Number = Number(walletCreationFee.amount) + Number(testWalletInitialFunds.amount);
-//
-//         await factoryClient.createWallet(
-//             {
-//                 createWalletMsg: {
-//                     user_pubkey: toBase64(userKeypair.pubkey),
-//                     guardians: {
-//                         addresses: [guardian1Addr!, guardian2Addr!],
-//                         // guardians_multisig: null,
-//                         guardians_multisig: {
-//                             threshold_absolute_count: 1,
-//                             multisig_initial_funds: [],
-//                         },
-//                     },
-//                     relayers: [relayer1Addr!, relayer2Addr!],
-//                     proxy_initial_funds: [testWalletInitialFunds as Coin],
-//                 },
-//             },
-//             defaultWalletCreationFee,
-//             undefined,
-//             [coin(totalFee.toString(), coinMinDenom!) as Coin]
-//         );
-//
-//         const { wallets } = await factoryClient.walletsOf({ user: userAddr! });
-//         return wallets[0];
-//     }
-//
-//     beforeAll(async () => {
-//         try {
-//             userClient = await createSigningClient(userMnemonic!, addrPrefix!);
-//             adminClient = await createSigningClient(adminMnemonic!, addrPrefix!);
-//             client = await CosmWasmClient.connect(rpcEndPoint!);
-//             const { factoryRes, proxyRes, multisigRes, govecRes } = await uploadContracts(adminClient);
-//             const { factoryAddr } = await instantiateFactoryContract(
-//                 adminClient,
-//                 factoryRes.codeId,
-//                 proxyRes.codeId,
-//                 multisigRes.codeId
-//             );
-//             factoryClient = new FactoryClient(adminClient, adminAddr!, factoryAddr);
-//             const { govecAddr } = await instantiateGovec(adminClient, govecRes.codeId, factoryAddr);
-//
-//             await factoryClient.updateGovecAddr({ addr: govecAddr });
-//
-//             let govec = await factoryClient.govecAddr();
-//             expect(govec).toEqual(govecAddr);
-//
-//             proxyWalletAddress = await createTestProxyWallet();
-//
-//             proxyClient = new ProxyClient(userClient, userAddr!, proxyWalletAddress);
-//         } catch (err) {
-//             console.error("Failed to load scenario!", err);
-//         }
-//     });
-//
-//     beforeEach(() => {
-//         assert(userClient);
-//         assert(adminClient);
-//         assert(client);
-//         assert(proxyWalletAddress);
-//         assert(factoryClient);
-//         assert(proxyClient);
-//     });
-//
-//     it("Should get correct info from proxy wallet", async () => {
-//         const info = await proxyClient.info();
-//         expect(info.guardians).toContain(guardian1Addr!);
-//         expect(info.guardians).toContain(guardian2Addr!);
-//         expect(info.relayers).toContain(relayer2Addr!);
-//         expect(info.relayers).toContain(relayer1Addr!);
-//         expect(info.is_frozen).toEqual(false);
-//         expect(info.multisig_address).toBeTruthy();
-//         expect(info.nonce).toEqual(0);
-//     });
-//
-//     it("User can use wallet to send funds", async () => {
-//         const sendAmount = coin(2, coinMinDenom!);
-//         const sendMsg: BankMsg = {
-//             send: {
-//                 to_address: adminAddr!,
-//                 amount: [sendAmount as Coin],
-//             },
-//         };
-//
-//         const walletBalanceBefore = await client.getBalance(proxyWalletAddress, coinMinDenom!);
-//         const adminBalanceBefore = await client.getBalance(adminAddr!, coinMinDenom!);
-//         await proxyClient.execute({
-//             msgs: [
-//                 {
-//                     bank: sendMsg,
-//                 },
-//             ],
-//         });
-//         const walletBalanceAfter = await client.getBalance(proxyWalletAddress, coinMinDenom!);
-//         const adminBalanceAfter = await client.getBalance(adminAddr!, coinMinDenom!);
-//
-//         const walletDiff = Number(walletBalanceBefore.amount) - Number(walletBalanceAfter.amount);
-//         const adminDiff = Number(adminBalanceBefore.amount) - Number(adminBalanceAfter.amount);
-//
-//         expect(walletDiff).toEqual(Number(sendAmount.amount));
-//         expect(adminDiff).toEqual(-Number(sendAmount.amount));
-//     });
-//
-//     it("User can send funds to wallet", async () => {
-//         const sendAmount = coin(10_000, coinMinDenom!);
-//         const userBalanceBefore = await client.getBalance(userAddr!, coinMinDenom!);
-//         const walletBalanceBefore = await client.getBalance(proxyWalletAddress, coinMinDenom!);
-//         await userClient.sendTokens(userAddr!, proxyWalletAddress, [sendAmount], defaultSendFee);
-//         const userBalanceAfter = await client.getBalance(userAddr!, coinMinDenom!);
-//         const walletBalanceAfter = await client.getBalance(proxyWalletAddress, coinMinDenom!);
-//
-//         const userDiff = Number(userBalanceBefore.amount) - Number(userBalanceAfter.amount);
-//         const walletDiff = Number(walletBalanceBefore.amount) - Number(walletBalanceAfter.amount);
-//
-//         // Gas price
-//         expect(userDiff).toBeGreaterThanOrEqual(Number(sendAmount.amount));
-//         expect(walletDiff).toEqual(-Number(sendAmount.amount));
-//     });
-//
-//     it("Should relay bank message as a relayer", async () => {
-//         const relayerClient = await createSigningClient(relayer1Mnemonic!, addrPrefix!);
-//         const relayerProxyClient = new ProxyClient(relayerClient, relayer1Addr!, proxyWalletAddress);
-//         const info = await relayerProxyClient.info();
-//
-//         const sendAmount = coin(10_000, coinMinDenom!);
-//         const sendMsg: CosmosMsg = {
-//             bank: {
-//                 send: {
-//                     to_address: adminAddr!,
-//                     amount: [sendAmount as Coin],
-//                 },
-//             },
-//         };
-//
-//         const relayTransaction = await createRelayTransaction(userMnemonic!, info.nonce, JSON.stringify(sendMsg));
-//         const walletBalanceBefore = await client.getBalance(proxyWalletAddress, coinMinDenom!);
-//
-//         await relayerProxyClient.relay(
-//             {
-//                 transaction: relayTransaction,
-//             },
-//             defaultRelayFee
-//         );
-//
-//         const walletBalanceAfter = await client.getBalance(proxyWalletAddress, coinMinDenom!);
-//         const diff = Number(walletBalanceBefore.amount) - Number(walletBalanceAfter.amount);
-//
-//         expect(sendAmount.amount).toEqual(String(diff));
-//         relayerClient.disconnect();
-//     });
-//
-//     it("Should relay WASM message as a relayer", async () => {
-//         const relayerClient = await createSigningClient(relayer1Mnemonic!, addrPrefix!);
-//         const relayerProxyClient = new ProxyClient(relayerClient, relayer1Addr!, proxyWalletAddress);
-//
-//         // Instantiate a new CW20 contract giving the wallet some funds
-//         const cw20Code = getContract(cw20CodePath!);
-//         const cw20Res = await adminClient.upload(adminAddr!, cw20Code, defaultUploadFee);
-//
-//         const initAmount = "1000";
-//         const cw20contract = await adminClient.instantiate(
-//             adminAddr!,
-//             cw20Res.codeId,
-//             {
-//                 name: "scw-test",
-//                 symbol: "scw",
-//                 decimals: 10,
-//                 initial_balances: [{ address: proxyWalletAddress, amount: initAmount }],
-//                 mint: null,
-//                 marketing: null,
-//             },
-//             "SCW Test CW20",
-//             defaultInstantiateFee,
-//             {
-//                 funds: [],
-//             }
-//         );
-//         const transferAmount = "100";
-//         const transferMsg = {
-//             transfer: { recipient: guardian1Addr!, amount: transferAmount },
-//         };
-//
-//         const cosmosWasmMsg: CosmosMsg = {
-//             wasm: {
-//                 execute: {
-//                     contract_addr: cw20contract.contractAddress,
-//                     msg: toBase64(toUtf8(JSON.stringify(transferMsg))),
-//                     funds: [],
-//                 },
-//             },
-//         };
-//
-//         const relayTransaction = await createRelayTransaction(userMnemonic!, 1, JSON.stringify(cosmosWasmMsg));
-//         await relayerProxyClient.relay(
-//             {
-//                 transaction: relayTransaction,
-//             },
-//             defaultExecuteFee
-//         );
-//
-//         const postFund = await relayerClient.queryContractSmart(cw20contract.contractAddress, {
-//             balance: { address: proxyWalletAddress },
-//         });
-//
-//         expect(postFund.balance).toEqual("900");
-//         relayerClient.disconnect();
-//     });
-//
-//     afterAll(() => {
-//         userClient?.disconnect();
-//         client?.disconnect();
-//     });
-// });
+describe("Proxy Suite: ", () => {
+    let userClient: SigningCosmWasmClient;
+    let adminClient: SigningCosmWasmClient;
+    let client: CosmWasmClient;
+    let proxyWalletAddress: Addr;
+
+    let factoryClient: FactoryClient;
+    let proxyClient: ProxyClient;
+
+    async function createTestProxyWallet(): Promise<Addr> {
+        assert(factoryClient);
+        const userKeypair = await mnemonicToKeyPair(userMnemonic!);
+        const walletCreationFee = await factoryClient.fee();
+        const totalFee: Number = Number(walletCreationFee.amount) + Number(testWalletInitialFunds.amount);
+
+        await factoryClient.createWallet(
+            {
+                createWalletMsg: {
+                    user_pubkey: toBase64(userKeypair.pubkey),
+                    guardians: {
+                        addresses: [guardian1Addr!, guardian2Addr!],
+                        // guardians_multisig: null,
+                        guardians_multisig: {
+                            threshold_absolute_count: 1,
+                            multisig_initial_funds: [],
+                        },
+                    },
+                    relayers: [relayer1Addr!, relayer2Addr!],
+                    proxy_initial_funds: [testWalletInitialFunds as Coin],
+                },
+            },
+            defaultWalletCreationFee,
+            undefined,
+            [coin(totalFee.toString(), coinMinDenom!) as Coin]
+        );
+
+        const { wallets } = await factoryClient.walletsOf({ user: userAddr! });
+        return wallets[0];
+    }
+
+    beforeAll(async () => {
+        try {
+            userClient = await createSigningClient(userMnemonic!, addrPrefix!);
+            adminClient = await createSigningClient(adminMnemonic!, addrPrefix!);
+            client = await CosmWasmClient.connect(rpcEndPoint!);
+            const { factoryRes, proxyRes, multisigRes, govecRes } = await uploadContracts(adminClient);
+            const { factoryAddr } = await instantiateFactoryContract(
+                adminClient,
+                factoryRes.codeId,
+                proxyRes.codeId,
+                multisigRes.codeId
+            );
+            factoryClient = new FactoryClient(adminClient, adminAddr!, factoryAddr);
+            const { govecAddr } = await instantiateGovec(adminClient, govecRes.codeId, factoryAddr);
+
+            await factoryClient.updateGovecAddr({ addr: govecAddr });
+
+            let govec = await factoryClient.govecAddr();
+            expect(govec).toEqual(govecAddr);
+
+            proxyWalletAddress = await createTestProxyWallet();
+
+            proxyClient = new ProxyClient(userClient, userAddr!, proxyWalletAddress);
+        } catch (err) {
+            console.error("Failed to load scenario!", err);
+        }
+    });
+
+    beforeEach(() => {
+        assert(userClient);
+        assert(adminClient);
+        assert(client);
+        assert(proxyWalletAddress);
+        assert(factoryClient);
+        assert(proxyClient);
+    });
+
+    it("Should get correct info from proxy wallet", async () => {
+        const info = await proxyClient.info();
+        expect(info.guardians).toContain(guardian1Addr!);
+        expect(info.guardians).toContain(guardian2Addr!);
+        expect(info.relayers).toContain(relayer2Addr!);
+        expect(info.relayers).toContain(relayer1Addr!);
+        expect(info.is_frozen).toEqual(false);
+        expect(info.multisig_address).toBeTruthy();
+        expect(info.nonce).toEqual(0);
+    });
+
+    it("User can use wallet to send funds", async () => {
+        const sendAmount = coin(2, coinMinDenom!);
+        const sendMsg: BankMsg = {
+            send: {
+                to_address: adminAddr!,
+                amount: [sendAmount as Coin],
+            },
+        };
+
+        const walletBalanceBefore = await client.getBalance(proxyWalletAddress, coinMinDenom!);
+        const adminBalanceBefore = await client.getBalance(adminAddr!, coinMinDenom!);
+        await proxyClient.execute({
+            msgs: [
+                {
+                    bank: sendMsg,
+                },
+            ],
+        });
+        const walletBalanceAfter = await client.getBalance(proxyWalletAddress, coinMinDenom!);
+        const adminBalanceAfter = await client.getBalance(adminAddr!, coinMinDenom!);
+
+        const walletDiff = Number(walletBalanceBefore.amount) - Number(walletBalanceAfter.amount);
+        const adminDiff = Number(adminBalanceBefore.amount) - Number(adminBalanceAfter.amount);
+
+        expect(walletDiff).toEqual(Number(sendAmount.amount));
+        expect(adminDiff).toEqual(-Number(sendAmount.amount));
+    });
+
+    it("User can send funds to wallet", async () => {
+        const sendAmount = coin(10_000, coinMinDenom!);
+        const userBalanceBefore = await client.getBalance(userAddr!, coinMinDenom!);
+        const walletBalanceBefore = await client.getBalance(proxyWalletAddress, coinMinDenom!);
+        await userClient.sendTokens(userAddr!, proxyWalletAddress, [sendAmount], defaultSendFee);
+        const userBalanceAfter = await client.getBalance(userAddr!, coinMinDenom!);
+        const walletBalanceAfter = await client.getBalance(proxyWalletAddress, coinMinDenom!);
+
+        const userDiff = Number(userBalanceBefore.amount) - Number(userBalanceAfter.amount);
+        const walletDiff = Number(walletBalanceBefore.amount) - Number(walletBalanceAfter.amount);
+
+        // Gas price
+        expect(userDiff).toBeGreaterThanOrEqual(Number(sendAmount.amount));
+        expect(walletDiff).toEqual(-Number(sendAmount.amount));
+    });
+
+    it("Should relay bank message as a relayer", async () => {
+        const relayerClient = await createSigningClient(relayer1Mnemonic!, addrPrefix!);
+        const relayerProxyClient = new ProxyClient(relayerClient, relayer1Addr!, proxyWalletAddress);
+        const info = await relayerProxyClient.info();
+
+        const sendAmount = coin(10_000, coinMinDenom!);
+        const sendMsg: CosmosMsg = {
+            bank: {
+                send: {
+                    to_address: adminAddr!,
+                    amount: [sendAmount as Coin],
+                },
+            },
+        };
+
+        const relayTransaction = await createRelayTransaction(userMnemonic!, info.nonce, JSON.stringify(sendMsg));
+        const walletBalanceBefore = await client.getBalance(proxyWalletAddress, coinMinDenom!);
+
+        await relayerProxyClient.relay(
+            {
+                transaction: relayTransaction,
+            },
+            defaultRelayFee
+        );
+
+        const walletBalanceAfter = await client.getBalance(proxyWalletAddress, coinMinDenom!);
+        const diff = Number(walletBalanceBefore.amount) - Number(walletBalanceAfter.amount);
+
+        expect(sendAmount.amount).toEqual(String(diff));
+        relayerClient.disconnect();
+    });
+
+    it("Should relay WASM message as a relayer", async () => {
+        const relayerClient = await createSigningClient(relayer1Mnemonic!, addrPrefix!);
+        const relayerProxyClient = new ProxyClient(relayerClient, relayer1Addr!, proxyWalletAddress);
+
+        // Instantiate a new CW20 contract giving the wallet some funds
+        const cw20Code = getContract(cw20CodePath!);
+        const cw20Res = await adminClient.upload(adminAddr!, cw20Code, defaultUploadFee);
+
+        const initAmount = "1000";
+        const cw20contract = await adminClient.instantiate(
+            adminAddr!,
+            cw20Res.codeId,
+            {
+                name: "scw-test",
+                symbol: "scw",
+                decimals: 10,
+                initial_balances: [{ address: proxyWalletAddress, amount: initAmount }],
+                mint: null,
+                marketing: null,
+            },
+            "SCW Test CW20",
+            defaultInstantiateFee,
+            {
+                funds: [],
+            }
+        );
+        const transferAmount = "100";
+        const transferMsg = {
+            transfer: { recipient: guardian1Addr!, amount: transferAmount },
+        };
+
+        const cosmosWasmMsg: CosmosMsg = {
+            wasm: {
+                execute: {
+                    contract_addr: cw20contract.contractAddress,
+                    msg: toBase64(toUtf8(JSON.stringify(transferMsg))),
+                    funds: [],
+                },
+            },
+        };
+
+        const relayTransaction = await createRelayTransaction(userMnemonic!, 1, JSON.stringify(cosmosWasmMsg));
+        await relayerProxyClient.relay(
+            {
+                transaction: relayTransaction,
+            },
+            defaultExecuteFee
+        );
+
+        const postFund = await relayerClient.queryContractSmart(cw20contract.contractAddress, {
+            balance: { address: proxyWalletAddress },
+        });
+
+        expect(postFund.balance).toEqual("900");
+        relayerClient.disconnect();
+    });
+
+    afterAll(() => {
+        userClient?.disconnect();
+        client?.disconnect();
+    });
+});
