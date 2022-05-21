9f7825cd605a8a2e6f5631011c38fc04ec170608
bwty <whalelephant@users.noreply.github.com>
2022-05-21 22:23:43 +0200

    chore: update schema

--- contracts/factory/examples/schema.rs
+++ contracts/factory/examples/schema.rs
@@ -17,7 +17,6 @@ fn main() {
     remove_schemas(&out_dir).unwrap();
 
     export_schema(&schema_for!(InstantiateMsg), &out_dir);
-    export_schema(&schema_for!(ExecuteMsg), &out_dir);
     export_schema(&schema_for!(WalletInfo), &out_dir);
     export_schema(&schema_for!(CreateWalletMsg), &out_dir);
     export_schema(&schema_for!(CodeIdType), &out_dir);
@@ -26,6 +25,7 @@ fn main() {
     export_schema(&schema_for!(WalletAddr), &out_dir);
     export_schema(&schema_for!(Cw20Coin), &out_dir);
     export_schema(&schema_for!(StakingOptions), &out_dir);
+    export_schema_with_title(&schema_for!(ExecuteMsg), &out_dir, "ExecuteMsg");
     export_schema_with_title(&schema_for!(QueryMsg), &out_dir, "QueryMsg");
     export_schema_with_title(&schema_for!(u64), &out_dir, "CodeIdResponse");
     export_schema_with_title(&schema_for!(Coin), &out_dir, "FeeResponse");
