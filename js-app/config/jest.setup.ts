87e43cb1ba5ad28d9017b6bf0718e236afce91f4
j0nl1 <jonlitech@gmail.com>
2022-05-29 21:15:11 +0200

    replace jest for jasmine

---
+++ js-app/config/jest.setup.ts
@@ -0,0 +1,5 @@
+import * as dotenv from "dotenv";
+
+const path = `${process.cwd()}/../.env`;
+
+dotenv.config({ path });
--- js-app/spec/helpers/helper.js
+++
@@ -1,8 +0,0 @@
-//-------------------------------------------------
-// This is needed for "enabling jasmine for ts"
-const TSConsoleReporter = require("jasmine-ts-console-reporter");
-jasmine.getEnv().clearReporters(); //Clear default console reporter
-jasmine.getEnv().addReporter(new TSConsoleReporter());
-// This is high because each suite may take some time and if it is too low
-// the suite will time out and start the next suite without warning.
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 600*1000;
