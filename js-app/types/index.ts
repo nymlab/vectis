1dc41ea6b93b58ab946736b901fdefbdc8e2be9c
j0nl1 <jonlitech@gmail.com>
2022-06-02 12:54:56 +0200

    feat: index file for better export in types folder

---
+++ js-app/types/index.ts
@@ -0,0 +1,5 @@
+import * as FactoryContract from "./FactoryContract";
+import * as GovecContract from "./GovecContract";
+import * as ProxyContract from "./ProxyContract";
+
+export default { ...FactoryContract, ...GovecContract, ...ProxyContract };
