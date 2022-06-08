d60fc7faf5c72fb46d9d9301e0f465c352959e04
j0nl1 <jonlitech@gmail.com>
2022-06-08 10:58:02 +0200

    feat: download contracts (cw-plus) currently

--- cli/core/utils/fs.ts
+++ cli/core/utils/fs.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 import path from "path";
-import { cachePath } from "./constants";
+import axios from "axios";
+import { cachePath, downloadContractPath } from "./constants";
 
 export function getContract(path: string): Uint8Array {
     return fs.readFileSync(path);
@@ -9,4 +10,26 @@ export function getContract(path: string): Uint8Array {
 export function writeInCacheFolder(fileName: string, content: string, encoding: BufferEncoding = "utf8"): void {
     if (!fs.existsSync(cachePath)) fs.mkdirSync(cachePath);
     fs.writeFileSync(path.join(cachePath, fileName), content, { encoding });
+}
+
+export async function downloadFile(url: string, fileName: string): Promise<void> {
+    const file = fs.createWriteStream(path.join(cachePath, fileName));
+
+    const { data } = await axios.get(url, { responseType: "stream" });
+    data.pipe(file);
+
+    return new Promise((resolve, reject) => {
+        file.on("finish", resolve);
+        file.on("error", reject);
+    });
+}
+
+export async function downloadContract(url: string, fileName: string): Promise<void> {
+    if (!fs.existsSync(downloadContractPath)) fs.mkdirSync(downloadContractPath);
+    await downloadFile(url, `contracts/${fileName}`);
+}
+
+export function areContractsDownloaded(): boolean {
+    if (!fs.existsSync(downloadContractPath)) return false;
+    return !!fs.readdirSync(downloadContractPath).length;
 }
