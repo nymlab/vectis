3a7679a5aa749a211722a796cd18c2c8aa89cff9
j0nl1 <jonlitech@gmail.com>
2022-06-13 13:07:29 +0200

    fix: update gov in deploy script

--- cli/core/deployer.ts
+++ cli/core/deployer.ts
@@ -1,8 +1,9 @@
 import { createSigningClient, downloadContracts, uploadContracts } from "./services/cosmwasm";
 import { instantiateFactoryContract } from "./services/factory";
 import { instantiateGovec } from "./services/govec";
-import { addrPrefix, adminMnemonic } from "./utils/constants";
+import { addrPrefix, adminAddr, adminMnemonic } from "./utils/constants";
 import { areContractsDownloaded, writeInCacheFolder } from "./utils/fs";
+import { FactoryClient } from "@vectis/types/contracts/FactoryContract";
 
 async function main() {
     if (!areContractsDownloaded()) await downloadContracts();
@@ -19,6 +20,10 @@ async function main() {
         multisigRes.codeId
     );
     const { govecAddr } = await instantiateGovec(adminClient, govecRes.codeId, factoryAddr);
+
+    const factoryClient = new FactoryClient(adminClient, adminAddr, factoryAddr);
+
+    await factoryClient.updateGovecAddr({ addr: govecAddr });
 
     console.log("Factory contract was instantiated at the following address:", factoryAddr);
 
