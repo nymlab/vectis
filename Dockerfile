eaec99c5b9434b10d3f2b8245e18423e6e730178
j0nl1 <jonlitech@gmail.com>
2022-06-21 19:22:51 +0200

    fix: docker image build

--- Dockerfile
+++ Dockerfile
@@ -18,7 +18,7 @@ RUN apk add --update nodejs npm
 
 WORKDIR /app/cli
 
-RUN junod start --rpc.laddr tcp://0.0.0.0:26657 & sleep 5 && npm ci && npm run test:ci && killall -9 junod
+RUN junod start --rpc.laddr tcp://0.0.0.0:26657 & sleep 5 && npm ci && npm run build:docker && killall -9 junod
 
 EXPOSE 1317
 EXPOSE 26656
--- cli/core/dao-deployer.ts
+++ cli/core/dao-deployer.ts
@@ -1,2 +1,9 @@
 import { deploy } from "./utils/dao-deploy";
-deploy();
+import { writeInCacheFolder } from "./utils/fs";
+
+async function deployContracts() {
+    const vectisDaoContractsAddrs = await deploy();
+    writeInCacheFolder("deployInfo.json", JSON.stringify(vectisDaoContractsAddrs, null, 2));
+}
+
+deployContracts();
