d5adf16984b2fbeaaeda6265d3c78d16034e1510
Dan Lynch <pyramation@gmail.com>
2022-08-09 16:05:34 -0700

    upgrade ts-codegen w/o bundling

--- cli/types/index.ts
+++ cli/types/index.ts
@@ -1,4 +1,3 @@
 export * as FactoryContract from "./contracts/Factory.client";
 export * as GovecContract from "./contracts/Govec.client";
 export * as ProxyContract from "./contracts/Proxy.client";
-export * from './contracts';
