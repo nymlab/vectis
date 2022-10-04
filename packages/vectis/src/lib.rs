2e3581fead45f7f10d2cb39f8f1d0e2ce4b439f9
j0nl1 <jonlitech@gmail.com>
2022-10-04 15:46:33 -0500

    feat: update guardian request

--- packages/vectis/src/lib.rs
+++ packages/vectis/src/lib.rs
@@ -1,15 +1,17 @@
 pub use crate::error::{MigrationMsgError, RelayTxError};
 pub use crate::factory::{
-    CodeIdType, CreateWalletMsg, Guardians, MultiSig, ProxyMigrateMsg, ProxyMigrationTxMsg,
+    CodeIdType, CreateWalletMsg, MultiSig, ProxyMigrateMsg, ProxyMigrationTxMsg,
     ThresholdAbsoluteCount, WalletFactoryExecuteMsg, WalletFactoryQueryMsg, WalletQueryPrefix,
 };
 pub use crate::govec::StakingOptions;
 pub use crate::pubkey::pub_key_to_address;
 pub use crate::signature::query_verify_cosmos;
 pub use crate::wallet::{Nonce, RelayTransaction, WalletAddr, WalletInfo};
+pub use crate::guardians::{Guardians, GuardiansUpdateRequest, GuardiansUpdateMsg};
 mod error;
 mod factory;
 mod govec;
 mod pubkey;
 mod signature;
 mod wallet;
+mod guardians;
