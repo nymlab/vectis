ac660235d030cdeaec33180ab9671588e035c49a
bwty <whalelephant@users.noreply.github.com>
2022-11-09 19:36:29 +0700

    feat: add 'transfer_from' for govec for proposal

--- contracts/govec/src/state.rs
+++ contracts/govec/src/state.rs
@@ -16,8 +16,13 @@ pub const TOKEN_INFO: Item<TokenInfo> = Item::new("token_info");
 pub const MARKETING_INFO: Item<MarketingInfoResponse> = Item::new("marketing_info");
 pub const LOGO: Item<Logo> = Item::new("logo");
 pub const BALANCES: Map<&Addr, Uint128> = Map::new("balance");
+pub const MINT_CAP: Item<Uint128> = Item::new("mint_cap");
+// updated by `UpdateConfigAddr`
 pub const STAKING_ADDR: Item<CanonicalAddr> = Item::new("staking_addr");
 pub const DAO_ADDR: Item<CanonicalAddr> = Item::new("DAO_addr");
 pub const DAO_TUNNEL: Item<CanonicalAddr> = Item::new("DAO_tunnel");
 pub const FACTORY: Item<CanonicalAddr> = Item::new("Factory");
-pub const MINT_CAP: Item<Uint128> = Item::new("mint_cap");
+
+// We update this with the DAO, we can query the DAO but this might run out of gas,
+// see https://github.com/nymlab/dao-contracts/blob/v1.0.0-vectis/contracts/cw-core/src/contract.rs#L517
+pub const PROP_MODULES: Map<Addr, ()> = Map::new("proposal_modules");
