9097f97b6192deabf67f4626c9031c331f64d68c
j0nl1 <jonlitech@gmail.com>
2022-11-16 17:50:52 +0100

    refactor: remove conditional unit test

--- contracts/remote_factory/src/tests.rs
+++ contracts/remote_factory/src/tests.rs
@@ -1,22 +1,17 @@
-use crate::{
-    contract::{
-        execute, instantiate, query_code_id, query_dao_addr, query_fees, query_unclaim_wallet_list,
-        CodeIdType,
-    },
-    error::ContractError,
-    msg::{ExecuteMsg, InstantiateMsg, UnclaimedWalletList},
-};
-#[cfg(test)]
 use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
-use cosmwasm_std::{coin, Coin, DepsMut};
-use {
-    crate::contract::query_pending_unclaim_wallet_list,
-    crate::msg::QueryMsg,
-    crate::state::GOVEC_CLAIM_LIST,
-    cosmwasm_std::Api,
-    cw_utils::{Expiration, DAY},
-    std::ops::{Add, Mul},
-    vectis_wallet::GOVEC_CLAIM_DURATION_DAY_MUL,
+use cosmwasm_std::{coin, Api, Coin, DepsMut};
+use std::ops::{Add, Mul};
+
+use cw_utils::{Expiration, DAY};
+
+use vectis_wallet::factory_queries::{query_dao_addr, query_unclaim_wallet_list};
+use vectis_wallet::GOVEC_CLAIM_DURATION_DAY_MUL;
+
+use crate::{
+    contract::{execute, instantiate, query_pending_unclaim_wallet_list},
+    error::ContractError,
+    msg::{ExecuteMsg, InstantiateMsg, QueryMsg},
+    state::GOVEC_CLAIM_LIST,
 };
 
 // this will set up the instantiation for other tests
@@ -120,7 +115,7 @@ fn handles_minted_submsg() {
     assert_eq!(claim_list.wallets.len(), 2);
 
     // wallet calls to be claimed
-    let info = mock_info("yes_wallet", &[]);
+    let info = mock_info("yes_wallet", &[coin(10u128, "ucosm")]);
     execute(deps.as_mut(), env.clone(), info, ExecuteMsg::ClaimGovec {}).unwrap();
 
     let claim_list = query_unclaim_wallet_list(deps.as_ref(), None, None).unwrap();
@@ -167,4 +162,52 @@ fn handles_minted_submsg() {
         .add(DAY.mul(GOVEC_CLAIM_DURATION_DAY_MUL))
         .unwrap();
     assert_eq!(claim_list.wallets[0].1, expiration);
+}
+
+#[test]
+fn admin_updates_addresses_work() {
+    let fee = coin(1, "ucosm");
+    let mut deps = mock_dependencies();
+    let initial_code_id = 1111;
+    do_instantiate(
+        deps.as_mut(),
+        initial_code_id,
+        initial_code_id,
+        "wasm",
+        fee.clone(),
+        coin(1, "ucosm"),
+        None,
+    );
+
+    let info = mock_info("admin", &[]);
+    let env = mock_env();
+
+    let msg = ExecuteMsg::UpdateGovecAddr {
+        addr: "new_govec".to_string(),
+    };
+
+    let err = execute(deps.as_mut(), env.clone(), info.clone(), msg).unwrap_err();
+    assert_eq!(err, ContractError::NotSupportedByChain {});
+
+    // update admin
+    let msg = ExecuteMsg::UpdateDao {
+        addr: "new_dao".to_string(),
+    };
+
+    let response = execute(deps.as_mut(), env.clone(), info.clone(), msg).unwrap();
+    assert_eq!(
+        response.attributes,
+        [("config", "DAO"), ("New DAO", "new_dao")]
+    );
+
+    let new_admin = query_dao_addr(deps.as_ref()).unwrap();
+    assert_eq!(new_admin, "new_dao");
+
+    // old admin cannot update addresses
+    let msg = ExecuteMsg::UpdateDao {
+        addr: "new_dao".to_string(),
+    };
+
+    let err = execute(deps.as_mut(), env, info, msg).unwrap_err();
+    assert_eq!(err, ContractError::Unauthorized {});
 }
