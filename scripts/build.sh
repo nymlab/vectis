50ace2633bdb0ebeec887c26d19a8c3188b3610f
j0nl1 <jonlitech@gmail.com>
2022-11-17 14:12:03 +0100

    feat: mv vectis-tests folder

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,5 @@
-OPTIMIZER_VER=0.12.6
-
-if [[ `uname -m` =~ "arm" ]]; then 
-OPTIMIZER_IMG=cosmwasm/workspace-optimizer-arm64; else 
-OPTIMIZER_IMG=cosmwasm/workspace-optimizer;
-fi
-
-cd ..
-
 docker run --rm -v "$(pwd)":/code \
+  --platform linux/amd64 \
   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-  ${OPTIMIZER_IMG}:${OPTIMIZER_VER}
-
-cd scripts
+ cosmwasm/workspace-optimizer:0.12.9
