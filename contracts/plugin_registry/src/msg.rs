78069c4a93ac63be20e1fad58c365984ddc8e8f2
j0nl1 <jonlitech@gmail.com>
2022-11-30 16:03:20 +0100

    chore: rebase main

--- contracts/plugin_registry/src/msg.rs
+++ contracts/plugin_registry/src/msg.rs
@@ -1,25 +1,16 @@
 use cosmwasm_schema::cw_serde;
-use cosmwasm_std::{CanonicalAddr, Coin};
+use cosmwasm_std::CanonicalAddr;
 
-// We can store information offchain like number of installs per plugin, review of plugins ?
 #[cw_serde]
 pub struct Plugin {
     pub id: u64,
     pub name: String,
     pub creator: CanonicalAddr,
-    // (Optional)
     pub ipfs_hash: String,
     // We enforce using semver for versioning
     pub version: String,
-    // Just one chain because could be different version between chains.
-    pub chain_id: String,
     pub code_id: u64,
     pub checksum: String,
-    // None means free
-    // shall we support different denoms than native?
-    pub price: Option<Coin>,
-    // permissions required by the plugin.
-    pub permissions: Vec<String>,
 }
 
 #[cw_serde]
@@ -40,10 +31,8 @@ pub enum ExecuteMsg {
         name: String,
         version: String,
         ipfs_hash: String,
-        chain_id: String,
         code_id: u64,
         checksum: String,
-        permissions: Vec<String>,
     },
     UnregisterPlugin {
         id: u64,
@@ -55,10 +44,6 @@ pub enum ExecuteMsg {
         ipfs_hash: Option<String>,
         code_id: Option<u64>,
         checksum: Option<String>,
-        permissions: Option<Vec<String>>,
-    },
-    BuyPlugin {
-        id: u64,
     },
     UpdateConfig {
         reviewers: Option<Vec<String>>,
@@ -70,11 +55,6 @@ pub enum ExecuteMsg {
 pub enum QueryMsg {
     GetConfig {},
     GetPlugins {
-        limit: Option<u32>,
-        start_after: Option<u32>,
-    },
-    GetPluginsByAddr {
-        addr: String,
         limit: Option<u32>,
         start_after: Option<u32>,
     },
