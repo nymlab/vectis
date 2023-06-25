5bbef9fe9950900c45e3b3e4dd391cc8aeae9e08
bwty <whalelephant@users.noreply.github.com>
2023-06-25 21:31:12 +0800

    chore: update contract and derived addr

--- cli/config/accounts/stargaze.ts
+++ cli/config/accounts/stargaze.ts
@@ -4,32 +4,32 @@ dotenv.config({ path: path.join(__dirname, "../../../.env") });
 
 const common = {
     admin: {
-        address: "star16g2rahf5846rxzp3fwlswy08fz8ccuwkcemxw5",
+        address: "stars16g2rahf5846rxzp3fwlswy08fz8ccuwkdlzjjf",
         mnemonic:
             "clip hire initial neck maid actor venue client foam budget lock catalog sweet steak waste crater broccoli pipe steak sister coyote moment obvious choose",
     },
     user: {
-        address: "star1tcxyhajlzvdheqyackfzqcmmfcr760magww7sn",
+        address: "stars1tcxyhajlzvdheqyackfzqcmmfcr760maagh2vw",
         mnemonic:
             "useful guitar throw awesome later damage film tonight escape burger powder manage exact start title december impulse random similar eager smart absurd unaware enlist",
     },
     guardian_1: {
-        address: "star1qwwx8hsrhge9ptg4skrmux35zgna47pwyl0xm9",
+        address: "stars1qwwx8hsrhge9ptg4skrmux35zgna47pw3ekj8c",
         mnemonic:
             "slim rely one tiny chapter job toilet vague moment inquiry abandon toe robust trust orchard oyster elephant jazz quantum shaft stairs polar drop gospel",
     },
     guardian_2: {
-        address: "star1wk2r0jrhuskqmhc0gk6dcpmnz094sc2ahkrh32",
+        address: "stars1wk2r0jrhuskqmhc0gk6dcpmnz094sc2azs6rdh",
         mnemonic:
             "prepare tired ten whisper receive spider heavy differ mom web champion clever brass sight furnace cash march rice use nature ginger portion area million",
     },
     relayer_1: {
-        address: "star1ucl9dulgww2trng0dmunj348vxneufu5cs2c6j",
+        address: "stars1ucl9dulgww2trng0dmunj348vxneufu5dknvx0",
         mnemonic:
             "regret blur gas upon blossom illness exercise lamp combine monster draw inquiry borrow scrub achieve credit country donate stool develop kid amount flush wall",
     },
     relayer_2: {
-        address: "star1yjammmgqu62lz4sxk5seu7ml4fzdu7gkkdhvsl",
+        address: "stars1yjammmgqu62lz4sxk5seu7ml4fzdu7gkrtwcvz",
         mnemonic:
             "material often similar patrol please flat van toast agree milk grass pause glow rhythm voyage reason potato sunset great govern pave decade critic lens",
     },
