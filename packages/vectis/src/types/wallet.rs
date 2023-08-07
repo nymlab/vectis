80ae1f1020ad5c3b5ba4b903618820639b5c4c00
bwty <whalelephant@users.noreply.github.com>
2023-08-07 17:18:51 +0800

    feat: update relay tx interface

--- packages/vectis/src/types/wallet.rs
+++ packages/vectis/src/types/wallet.rs
@@ -4,7 +4,7 @@ use crate::{
     ProxyAddrErr, RelayTxError,
 };
 use cosmwasm_schema::cw_serde;
-use cosmwasm_std::{Addr, Binary, CanonicalAddr, CosmosMsg};
+use cosmwasm_std::{Addr, Binary, CanonicalAddr, Coin, CosmosMsg};
 use cw_utils::Threshold;
 
 /// The main controller of the account
@@ -94,20 +94,40 @@ pub struct RelayTransaction {
     /// Message to verify,
     /// Encoding depends on the authenticator,
     /// but must contain the actual Vec<CosmosMsg> to execute
-    /// e.g. the structure of CosmosRelayedTx Msg
+    /// e.g. the structure of CosmosRelayedTxMsg or WebauthnRelayedTxMsg
     pub message: Binary,
-    /// Nonce
-    pub nonce: Nonce,
     /// Serialized signature (message + nonce).
     /// If authenticator is CosmosEOA: 64 bytes - secp256k1 verification scheme
     /// See `AuthenticatorType` for more info
     pub signature: Binary,
 }
 
+/// Data to be signed by the controlling entity
+#[cw_serde]
+pub struct VectisRelayedTx {
+    /// messages to be executed on the entity's behalf
+    pub messages: Vec<CosmosMsg>,
+    /// nonce of the entity for relayed tx
+    pub nonce: Nonce,
+    /// fee for the relaying party
+    pub sponsor_fee: Option<Coin>,
+}
+
+/// The struct that RelayTransaction.message should decode to
+/// for CosmosEOA
 #[cw_serde]
 pub struct CosmosRelayedTxMsg {
-    pub messages: Vec<CosmosMsg>,
-    pub nonce: Nonce,
+    pub signed_data: VectisRelayedTx,
+}
+
+/// The struct that RelayTransaction.message should decode to
+/// for Webauthn
+#[cw_serde]
+pub struct WebauthnRelayedTxMsg {
+    /// the data to be signed, aka, the challenge
+    pub signed_data: VectisRelayedTx,
+    pub auth_data: Binary,
+    pub client_data: Binary,
 }
 
 #[cw_serde]
