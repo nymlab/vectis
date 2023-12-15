5053d8d91fca9395efb96a4961a7e81e9ad5bd3f
bwty <whalelephant@users.noreply.github.com>
2023-12-15 20:23:30 +0800

    chore: schemas rust & ts

--- scripts/schemas.sh
+++ scripts/schemas.sh
@@ -3,3 +3,14 @@ for dir in $PWD/contracts/core/*/; do
  cargo run
  cd -
 done
+
+for dir in $PWD/contracts/authenticators/*/; do
+ cd $dir
+ cargo run
+ cd -
+done
+
+cd ts
+npm i
+npm run generate
+cd ..
