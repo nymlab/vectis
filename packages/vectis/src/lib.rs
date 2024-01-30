0c18bd591769a0ee572498dd304990bf07b5b50e
bwty <whalelephant@users.noreply.github.com>
2024-01-30 15:23:21 -0800

    chore: remove vectis-wallet types

--- packages/vectis/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#![allow(deprecated)]
-pub mod interface;
-pub mod types;
-pub mod util;
-
-//  Global settings for addr pagination
-pub const MAX_LIMIT: u32 = 100;
-pub const DEFAULT_LIMIT: u32 = 50;
-pub const POLICY: &[u8] = "vectis-policy".as_bytes();
