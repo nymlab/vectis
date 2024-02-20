74a679719d2ac43be9e77ecc3ffdfd24f294afab
whalelephant <whalelephant@users.noreply.github.com>
2024-02-20 05:58:08 +0000

    deploy: fc2a618567cc6212320056cb188f96ab597b1c5d

--- trait.impl/core/default/trait.Default.js
+++ trait.impl/core/default/trait.Default.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"vectis_wallet":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.75.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"vectis_wallet/types/plugin_registry/struct.Subscriber.html\" title=\"struct vectis_wallet::types::plugin_registry::Subscriber\">Subscriber</a>"]]
+"vectis_wallet":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.76.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"vectis_wallet/types/plugin_registry/struct.Subscriber.html\" title=\"struct vectis_wallet::types::plugin_registry::Subscriber\">Subscriber</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
